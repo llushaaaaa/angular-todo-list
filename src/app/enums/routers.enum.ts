a5597330f5a43f57d102baa298de93d29579eaff
ilya.zharikov <ilya.zharikov@medvidi.com>
2024-04-27 08:47:25 +0300

    added routers and titles enums

---
+++ src/app/enums/routers.enum.ts
@@ -0,0 +1,5 @@
+export enum ERoutes {
+  LIST = 'list',
+  FAVORITE = 'favorite',
+  ADD = 'add',
+}
---
+++ src/app/enums/titles.enum.ts
@@ -0,0 +1,5 @@
+export enum ETitles {
+    TODO_LIST = 'Todo List',
+    TODO_FAVORITE_LIST = 'Todo Favorite List',
+    TODO_ADD = 'Todo Add',
+}  
