1c9c735ea37fe1fa65c3dbf2c997c0de90d86220
ilya.zharikov <ilya.zharikov@medvidi.com>
2024-04-27 08:50:51 +0300

    add header and pages in the template, created router paths and added lazy loading pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/header/header.component';
+import { Subject, filter, takeUntil } from 'rxjs';
+import { ETitles } from '@enums/titles.enum';
+import { ERoutes } from '@enums/routers.enum';
+
+const TITLES: Record<ERoutes, ETitles> = {
+  [ERoutes.LIST]: ETitles.TODO_LIST,
+  [ERoutes.ADD]: ETitles.TODO_ADD,
+  [ERoutes.FAVORITE]: ETitles.TODO_FAVORITE_LIST,
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit, OnDestroy {
+  public title = 'Todo List';
+
+  private destroy$ = new Subject<void>();
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.subscribeRouterEvents();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private subscribeRouterEvents(): void {
+    this.router.events
+      .pipe(
+        takeUntil(this.destroy$),
+        filter((e) => e instanceof NavigationEnd)
+      )
+      .subscribe((e) => {
+        const { urlAfterRedirects } = e as NavigationEnd;
+        this.title = TITLES[urlAfterRedirects.replace('/', '') as ERoutes];
+      });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { ERoutes } from '@enums/routers.enum';
+import { ETitles } from '@enums/titles.enum';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  {
+    path: ERoutes.LIST,
+    title: ETitles.TODO_LIST,
+    loadChildren: () =>
+      import('./pages/todo-list/todo-list.module').then(
+        (m) => m.TodoListModule
+      ),
+  },
+  {
+    path: ERoutes.FAVORITE,
+    title: ETitles.TODO_FAVORITE_LIST,
+    loadChildren: () =>
+      import('./pages/todo-favorite-list/todo-favorite-list.module').then(
+        (m) => m.TodoFavoriteListModule
+      ),
+  },
+  {
+    path: ERoutes.ADD,
+    title: ETitles.TODO_ADD,
+    loadChildren: () =>
+      import('./pages/todo-add/todo-add.module').then((m) => m.TodoAddModule),
+  },
+];
