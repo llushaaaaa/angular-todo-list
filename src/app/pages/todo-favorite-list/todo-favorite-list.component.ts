880fa7bec3c24afbcdb3f4fd5384c745d7ff21cd
ilya.zharikov <ilya.zharikov@medvidi.com>
2024-04-28 15:46:28 +0300

    added widget-with-todos on the todo-favorite-list page

--- src/app/pages/todo-favorite-list/todo-favorite-list.component.ts
+++ src/app/pages/todo-favorite-list/todo-favorite-list.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { WidgetWithTodosComponent } from '@components/widget-with-todos/widget-with-todos.component';
 
 @Component({
   selector: 'app-todo-favorite-list',
+  standalone: true,
+  imports: [WidgetWithTodosComponent],
   templateUrl: './todo-favorite-list.component.html',
-  styleUrls: ['./todo-favorite-list.component.css']
+  styleUrls: ['./todo-favorite-list.component.scss'],
 })
-export class TodoFavoriteListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class TodoFavoriteListComponent {}
